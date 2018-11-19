13ac054ca943269978dd39e736a3637477d29da1
leonpater <fhanissc@live.com>
2018-11-19 13:52:00 +0800

    add

---
+++ src/main/java/com/lp/share/base/BaseConstants.java
@@ -0,0 +1,15 @@
+package com.lp.share.base;
+
+import com.lp.share.util.LpStringUtils;
+
+public class BaseConstants {
+	
+	public class SignCst {
+		public static final String BLANK = LpStringUtils.EMPTY;
+		public static final String EMPTY = LpStringUtils.EMPTY;
+	}
+	
+	private BaseConstants() {
+		
+	}
+}
